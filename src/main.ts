df3edd0c2426c94251d2c9f8ea8ca5a5d89fd4bd
xiaolin <462441355@qq.com>
2022-05-14 21:29:31 +0800

    引入vant

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router/index'
 import { createPinia } from 'pinia'
 import 'lib-flexible'
+import { setupPlugins } from './plugins';
 
 const app = createApp(App)
+
+// 安装插件（vant-ui等）,若使用了 vite-plugin-components 插件，则需要手动引入组件
+setupPlugins(app);
 
 app.use(router)
 app.use(createPinia())
--- src/pages/index/Index.vue
+++ src/pages/index/Index.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
-        index
+        <van-button type="primary">Primary</van-button>
     </div>
 </template>
+
+<script lang="ts">
+
+</script>
