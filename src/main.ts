3cbc278607f580d1532525fbeb27e2369e7f4641
xiaolin <462441355@qq.com>
2022-05-16 10:03:31 +0800

    vuex

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import router from './router/index'
 import { createPinia } from 'pinia'
 import 'lib-flexible'
 import { setupPlugins } from './plugins';
+import { setupStore } from './store';
 
 const app = createApp(App)
 
 // 安装插件（vant-ui等）,若使用了 vite-plugin-components 插件，则需要手动引入组件
 setupPlugins(app);
-
+// 安装vuex
+setupStore(app);
 app.use(router)
 app.use(createPinia())
 
