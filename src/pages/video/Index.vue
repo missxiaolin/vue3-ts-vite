b9c6339d4d1fd13a51d25f54779579a6d94381c6
xiaolin <462441355@qq.com>
2022-05-29 21:05:53 +0800

    video

--- src/pages/video/Index.vue
+++ src/pages/video/Index.vue
@@ -1,33 +1,53 @@
 <template>
   <div class="example">
-    <Yvideo :src="src" :poster="poster">
-      <div class="mantle">
-        <div class="right" @click.stop="">
-          <div class="right-btn fabulous" @click="fabulous">点赞</div>
-          <div class="right-btn comment" @click="comment">评论</div>
-          <div class="right-btn collection" @click="collection">收藏</div>
-          <div class="right-btn share" @click="share">分享</div>
+    <Yslide
+      :list="data"
+      v-slot="{ item, index, activeIndex }"
+      @refresh="refresh"
+      @toBottom="toBottom"
+      @load="load"
+    >
+      <Yvideo
+        :src="item.entStoreVO.video"
+        :poster="item.entStoreVO.videoImg"
+        :index="index"
+        :activeIndex="activeIndex"
+        autoplay
+      >
+        <div class="mantle">
+          <div class="right" @click.stop="">
+            <div class="right-btn fabulous" @click="fabulous">点赞</div>
+            <div class="right-btn comment" @click="comment">评论</div>
+            <div class="right-btn collection" @click="collection">收藏</div>
+            <div class="right-btn share" @click="share">分享</div>
+          </div>
         </div>
-      </div>
-    </Yvideo>
+      </Yvideo>
+    </Yslide>
   </div>
   <div class="bottom">这是底部</div>
 </template>
 
 <script lang="ts">
-import { Yvideo } from '../../components/index'
+import { Yslide, Yvideo } from '../../components/index'
 import { defineComponent, ref, computed } from 'vue'
+import dataJSON from '../../mock/data.json'
+
 export default defineComponent({
   components: {
+    Yslide,
     Yvideo,
   },
   setup() {
-    const src = ref(
-      'https://wos2.58cdn.com.cn/DeFazYxWvDti/frsupload/d49662adaebeda888972bb3703e8a375.mp4'
-    )
-    const poster = ref(
-      'https://pic3.58cdn.com.cn/nowater/frs/n_v22f8aefa579b5450997ae6b867b145d08.jpg'
-    )
+    function refresh() {
+      console.log('表示上拉刷新')
+    }
+    function toBottom() {
+      console.log('表示已到底部')
+    }
+    function load() {
+      console.log('表示加载预加载下一个')
+    }
     function fabulous() {
       console.log('这是点赞')
     }
@@ -41,8 +61,10 @@ export default defineComponent({
       console.log('这是分享')
     }
     return {
-      src,
-      poster,
+      data: dataJSON.data.enterprises,
+      refresh,
+      toBottom,
+      load,
       fabulous,
       comment,
       collection,
